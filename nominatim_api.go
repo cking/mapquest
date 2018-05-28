191815deed501d25f36fe1c18d37b03b5238c4d5
Ch. König <criss@paars.de>
2018-05-28 10:56:54 +0200

    add nominatim api

--- nominatim_api.go
+++ nominatim_api.go
@@ -1,69 +1,94 @@
 package mapquest
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
+	"net/http"
 	"net/url"
 
 	"github.com/google/go-querystring/query"
 )
 
-var _ = log.Print
-
 const (
-	// NominatimPathPrefix is the default path prefix for the Nominatim API.
-	NominatimPathPrefix = "/nominatim/v1"
+	NominaatimPrefix  = "nominatim"
+	NominaatimVersion = "v1"
 )
 
-// NominatimAPI is a geographic search service that relies solely on the
-// data contributed to OpenStreetMap.
-// See http://open.mapquestapi.com/nominatim/ for details.
+// NominatimAPI enables users to request nominatim searches via the
+// MapQuest API. See https://developer.mapquest.com/documentation/open/nominatim-search/ for details.
 type NominatimAPI struct {
 	c *Client
 }
 
-// Search searches for details given an address.
+func (api *NominatimAPI) SimpleSearch(query string, limit int) (*NominatimSearchResponse, error) {
+	if limit < 0 {
+		limit = 0
+	}
+	return api.Search(&NominatimSearchRequest{Query: query, Limit: limit})
+}
+
 func (api *NominatimAPI) Search(req *NominatimSearchRequest) (*NominatimSearchResponse, error) {
-	u, err := api.buildSearchURL(req)
+	q, err := query.Values(req)
 	if err != nil {
 		return nil, err
 	}
 
-	res := new(NominatimSearchResponse)
-	res.Results = make([]*NominatimSearchResult, 0)
+	q.Set("key", api.c.key)
+	q.Set("format", "json")
+	u := apiURL(StaticMapPrefix, StaticMapVersion, "search.api")
+	u.RawQuery = q.Encode()
 
-	if err := api.c.getJSON(u, &res.Results); err != nil {
+	httpRequest, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	httpRequest.Header.Set("User-Agent", UserAgent)
+	httpResponse, err := api.c.httpClient.Do(httpRequest)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResponse.Body.Close()
+
+	var res *NominatimSearchResponse
+	if err := json.NewDecoder(httpResponse.Body).Decode(&res); err != nil {
 		return nil, err
 	}
 
 	return res, nil
 }
 
-// buildSearchURL returns the complete URL for the request,
-// including the key to query the MapQuest API.
-func (api *NominatimAPI) buildSearchURL(req *NominatimSearchRequest) (string, error) {
-	urls := fmt.Sprintf("%s%s/search.php", api.c.BaseURL(), NominatimPathPrefix)
-	u, err := url.Parse(urls)
+func (api *NominatimAPI) SimpleReverse(lat, long float64) (*NominatimSearchResponseEntry, error) {
+	return api.Reverse(&NominatimReverseRequest{Latitude: lat, Longitude: long})
+}
+
+func (api *NominatimAPI) Reverse(req *NominatimReverseRequest) (*NominatimSearchResponseEntry, error) {
+	q, err := query.Values(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	// Add key and other parameters to the query string
-	if req.Query != "" {
-		req.Street = ""
-		req.City = ""
-		req.County = ""
-		req.State = ""
-		req.Country = ""
-		req.PostalCode = ""
-	}
-	v, _ := query.Values(req)
+	q.Set("key", api.c.key)
+	q.Set("format", "json")
+	u := apiURL(StaticMapPrefix, StaticMapVersion, "search.api")
+	u.RawQuery = q.Encode()
 
-	v.Set("format", "json")
-	v.Set("addressdetails", "1")
-	v.Set("key", api.c.key)
-	u.RawQuery = v.Encode()
-	return u.String(), nil
+	httpRequest, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	httpRequest.Header.Set("User-Agent", UserAgent)
+	httpResponse, err := api.c.httpClient.Do(httpRequest)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResponse.Body.Close()
+
+	var res *NominatimSearchResponseEntry
+	if err := json.NewDecoder(httpResponse.Body).Decode(&res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
 
 type NominatimViewBox struct {
@@ -73,34 +98,36 @@ type NominatimViewBox struct {
 	Bottom float64
 }
 
-func (vb *NominatimViewBox) EncodeValues(key string, v *url.Values) error {
-	v.Set(key, fmt.Sprintf("%f,%f,%f,%f", vb.Left, vb.Top, vb.Right, vb.Bottom))
+func (s *NominatimViewBox) EncodeValues(key string, v *url.Values) error {
+	v.Set(key, fmt.Sprintf("%f,%f,%f,%f", s.Left, s.Top, s.Right, s.Bottom))
 	return nil
 }
 
+type NominatimOSMType string
+
+const (
+	OSMTypeNode     NominatimOSMType = "N"
+	OSMTypeWay                       = "W"
+	OSMTypeRelation                  = "R"
+)
+
 type NominatimSearchRequest struct {
-	Query           string            `url:"q,omitempty"`
-	Street          string            `url:"street,omitempty"`
-	City            string            `url:"city,omitempty"`
-	County          string            `url:"county,omitempty"`
-	State           string            `url:"state,omitempty"`
-	Country         string            `url:"country,omitempty"`
-	PostalCode      string            `url:"postalcode,omitempty"`
+	Query           string            `url:"q"`
+	AddressDetails  bool              `url:"addressdetails,omitempty"`
 	Limit           int               `url:"limit,omitempty"`
-	CountryCodes    []string          `url:"coutrycodes,comma,omitempty"`
-	ViewBox         *NominatimViewBox `url:"viewbox,omitempty"`
-	ExcludePlaceIds []string          `url:"exclude_place_ids,comma,omitempty"`
-	Bounded         *bool             `url:"bounded,omitempty"`
-	RouteWidth      *float64          `url:"routewidth,omitempty"`
-	OSMType         string            `url:"osm_type,omitempty"`
-	OSMId           string            `url:"osm_id,omitempty"`
+	CountryCodes    []string          `url:"countrycodes,comma,omitempty"`
+	ViewBox         *NominatimViewBox `url:"viewbox,omitempty"` // let,top,right,bottom => todo
+	ExcludePlaceIDs []string          `url:"exclude_place_ids,comma,omitempty"`
+	RouteWidth      float64           `url:"routewidth,omitempty"`
+	OSMType         NominatimOSMType  `url:"osm_type,omitempty"`
+	OSMID           string            `url:"osm_id,omitempty"`
 }
 
 type NominatimSearchResponse struct {
-	Results []*NominatimSearchResult
+	Results []*NominatimSearchResponseEntry
 }
 
-type NominatimSearchResult struct {
+type NominatimSearchResponseEntry struct {
 	Address *struct {
 		City          string `json:"city,omitempty"`
 		CityDistrict  string `json:"city_district,omitempty"`
@@ -118,15 +145,23 @@ type NominatimSearchResult struct {
 		StateDistrict string `json:"state_district,omitempty"`
 		Suburb        string `json:"suburb,omitempty"`
 	} `json:"address,omitempty"`
-	//BoundingBox []float64 `json:"boundingbox,omitempty"`
-	Class       string  `json:"class,omitempty"`
-	DisplayName string  `json:"display_name,omitempty"`
-	Importance  float64 `json:"importance,omitempty"`
-	Latitude    float64 `json:"lat,string,omitempty"`
-	Longitude   float64 `json:"lon,string,omitempty"`
-	OSMId       string  `json:"osm_id,omitempty"`
-	OSMType     string  `json:"osm_type,omitempty"`
-	PlaceId     string  `json:"place_id,omitempty"`
-	Type        string  `json:"type,omitempty"`
-	License     string  `json:"licence,omitempty"` // typo in API?
+	BoundingBox []float64 `json:"boundingbox,omitempty"`
+	Class       string    `json:"class,omitempty"`
+	DisplayName string    `json:"display_name,omitempty"`
+	Importance  float64   `json:"importance,omitempty"`
+	Latitude    float64   `json:"lat,string,omitempty"`
+	Longitude   float64   `json:"lon,string,omitempty"`
+	OSMId       string    `json:"osm_id,omitempty"`
+	OSMType     string    `json:"osm_type,omitempty"`
+	PlaceID     string    `json:"place_id,omitempty"`
+	Type        string    `json:"type,omitempty"`
+	License     string    `json:"licence,omitempty"` // typo in API
+	Icon        string    `json:"icon,omitempty"`
+}
+
+type NominatimReverseRequest struct {
+	Latitude  float64          `url:"lat"`
+	Longitude float64          `url:"long"`
+	OSMType   NominatimOSMType `url:"osm_type,omitempty"`
+	OSMID     string           `url:"osm_id,omitempty"`
 }
